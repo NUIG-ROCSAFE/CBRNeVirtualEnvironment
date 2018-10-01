d185b4bdc78034d0556b51606a705f33c00456cc
accasio <accasio@gmail.com>
2018-10-01 16:20:45 +0100

    airsim project settings file added if it doesn't exist

---
+++ PythonCode/gen_airsim.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import shutil
+
+if not os.path.isdir(os.path.expanduser("~/AirSim/")):
+    os.makedirs(os.path.expanduser("~/AirSim/"))
+
+if not os.path.exists(os.path.expanduser("~/AirSim/settings.json")):
+    shutil.copy(sys.argv[1], os.path.expanduser("~/AirSim/settings.json"))
